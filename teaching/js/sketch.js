6c04c8ecd6827e67ba67d599830bfe85649f8e3e
joelgoughbecker <116875565+joelgoughbecker@users.noreply.github.com>
2022-11-25 12:59:18 +0000

    Add files via upload

---
+++ teaching/js/sketch.js
@@ -0,0 +1,32 @@
+
+function setup(){
+  var canvas = createCanvas(800,800);
+  canvas.parent("p5container");
+
+  //for(var i = 0; i 0 <10; i++){
+  //  console.log('I want ' + i + '  Ice Cream!')
+  //}
+  
+
+  function draw(){
+    background(200)
+
+    stroke(255);
+
+   // for(var lineX = 75; lineX <= 255; lineX += 75){
+    //  line(lineX, 0 ,lineX, height);
+
+   // for(var x = 0; x <= 80; x++){    row of circles
+     // circle(x*10, height/2,10)
+
+
+     for(var y = 0; y<=80; y++){
+       for(var x = 0; x<=80; x++){}
+     }
+
+
+    }
+    }
+  
+  
+
